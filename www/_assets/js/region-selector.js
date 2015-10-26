56785fe2bb86e8c84d82d9201d164f1e3ddb093a
Henry Charge <henry.charge@valtech.co.uk>
2015-10-26 11:10:33 +0000

    Updating select list by choosing location on map

--- www/_assets/js/region-selector.js
+++ www/_assets/js/region-selector.js
@@ -3,8 +3,7 @@ $(function() {
   var $mostRecentRegion = '',
       $selectedRegion = '',
       $selectedRegionMap = '',
-      $selectedRegionName = '',
-      selectedRegionLocations = [];
+      $selectedRegionName = '';
 
 
 
@@ -18,6 +17,7 @@ $(function() {
     $('#hoveredRegionName').text($regionName);
 
   });
+
 
 
   $('.svg-map').on('mouseleave', function() {
@@ -34,61 +34,64 @@ $(function() {
   $('.region-container').on('click', function() {
     var $this = $(this),
         $regionNameID = $this.attr('id'),
-        $regEl = $('.map-region[data-region="' + $regionNameID + '"]');
+        $regEl = $('#regionSelect optgroup[data-optregion="' + $regionNameID + '"]');
 
     selectRegion($this, $regEl);
   });
 
 
+//   function selectRegion(regionContainer, regionElement) {
+//     var regionName = regionElement.find('.region-name').text(),
+//     selectedLocationLinks = [];
+
+//     $selectedRegion = regionContainer;
+//     $selectedRegionMap = regionElement;
+
+//     $('#selectLocationBlurb').hide().attr('aria-hidden', true);
+//     $('#locationSelectedContainer').removeClass('hidden').attr('aria-hidden', false);
+//     regionContainer.attr('class', 'region-container selected');
+
+//     $('#hoveredRegionName, #regionNameHeading').text(regionName);
+//     $selectedRegionName = regionName;
+
+//     $('#regionLocations').html('');
+//     $('#regionLocationBlurb').html('');
+//     selectedRegionLocations = [];
 
 
-  $('#regionSelect').on('change', function() {
-    var $this = $(this),
-        $thisVal = $this.val(),
-        $theAnchor = $('.map-region[data-region="' + $thisVal + '"]'),
-        $theRegion = $('.region-container#' + $thisVal + '');
+//     $('#regionLocationBlurb').append(selectedLocationLinks.join(', '));
 
-    selectRegion($theRegion, $theAnchor);
-  });
+//     $('.map-region').not($selectedRegionMap).hide();
+//     $('.region-container').not($selectedRegion).attr('class', 'region-container');
+//   }
 
+// });
 
 
-
-  function selectRegion(regionContainer, regionAnchor) {
-    var regionName = regionAnchor.find('.region-name').text(),
-    selectedLocationLinks = [];
+function selectRegion(regionContainer, regionElement) {
+    var regionName = regionElement.attr('label');
 
     $selectedRegion = regionContainer;
-    $selectedRegionMap = regionAnchor;
+    $selectedRegionMap = regionElement;
 
-    regionAnchor.show();
     $('#selectLocationBlurb').hide().attr('aria-hidden', true);
     $('#locationSelectedContainer').removeClass('hidden').attr('aria-hidden', false);
     regionContainer.attr('class', 'region-container selected');
 
-    $('#hoveredRegionName, #regionNameHeading').text(regionName);
+    $('#hoveredRegionName').text(regionName);
     $selectedRegionName = regionName;
 
-    $('#regionLocations').html('');
-    $('#regionLocationBlurb').html('');
-    selectedRegionLocations = [];
-
-    regionAnchor.find('.region-city').each(function() {
-      var $locationName = $.trim($(this).text());
-
-      $('#regionLocations').append('<li>' + $locationName + '</li>');
-      selectedRegionLocations.push('<li>' + $locationName + '</li>');
-      selectedLocationLinks.push('<a class="link-unimp" href="https://maps.google.com" target="_blank">' + $locationName + '</a>');
-
-    });
-
-    $('#regionLocationBlurb').append(selectedLocationLinks.join(', '));
-
-    $('.map-region').not($selectedRegionMap).hide();
     $('.region-container').not($selectedRegion).attr('class', 'region-container');
+
+    regionElement.removeClass('hidden').find('option').show();
+    $('[data-optregion]').not(regionElement).addClass('hidden').find('option').hide();
+
+    $('#regionSelect').trigger("chosen:updated");
   }
 
 });
+
+
 
 
 
