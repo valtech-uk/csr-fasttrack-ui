f4effc4db25e14ae6645a44b8801b5d54c337636
Henry Charge <henry.charge@valtech.co.uk>
2015-10-27 09:44:56 +0000

    Remove chosen from framework page

--- www/_assets/js/interactions.js
+++ www/_assets/js/interactions.js
@@ -379,13 +379,4 @@ $(function() {
 
   $('.chosen-select').chosen({width: '100%'});
 
-  // Chosen touch support.
-    if ($('.chosen-container').length > 0) {
-      $('.chosen-container').on('touchstart', function(e){
-        e.stopPropagation(); e.preventDefault();
-        // Trigger the mousedown event.
-        $(this).trigger('mousedown');
-      });
-    }
-
 });
